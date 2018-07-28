9f7ce1e7b852876692b7e53db014939a045a3956
virtmerlin <merlin@sense.net>
2018-07-28 14:27:56 -0500

    Clean certgen

--- deployment/webhook-create-signed-cert.sh
+++ deployment/webhook-create-signed-cert.sh
@@ -7,7 +7,7 @@ usage() {
 Generate certificate suitable for use with an sidecar-injector webhook service.
 
 This script uses k8s' CertificateSigningRequest API to a generate a
-certificate signed by k8s CA suitable for use with sidecar-injector webhook
+certificate signed by k8s CA suitable for use with the webhook
 services. This requires permissions to create and approve CSR. See
 https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for
 detailed explantion and additional instructions.
@@ -46,7 +46,7 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-[ -z ${service} ] && service=setvn-webhook-svc
+[ -z ${service} ] && service=setenv-webhook-svc
 [ -z ${secret} ] && secret=setenv-webhook-certs
 [ -z ${namespace} ] && namespace=default
 
